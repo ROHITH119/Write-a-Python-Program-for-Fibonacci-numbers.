3e9fa89e1d4c58de3e20afa15b81876e3590ee29
ROHITH119 <77120336+ROHITH119@users.noreply.github.com>
2021-06-28 14:42:25 +0530

    create Write a Python Program for Fibonacci numbers.py
    mycaptain

---
+++ Write a Python Program for Fibonacci numbers.py
@@ -0,0 +1,14 @@
+n1 =0
+n2 =1
+nterms = int(input("enter no of fibonacci terms to be displayed : "))
+if nterms<=0 :
+     print("Please enter a positive integer")
+elif nterms == 1:
+    print("Fibonacci sequence upto",nterms,":")
+    print(n1)
+else :
+    for i in range(nterms):
+        print(n1)
+        x = n1 + n2
+        n1 = n2
+        n2 = x
